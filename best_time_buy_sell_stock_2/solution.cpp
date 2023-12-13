011bafe5db56bdde2abed3d5cb695f6120f7f579
TravHaran <ratnahat@mcmaster.ca>
2023-12-13 17:16:06 -0500

    completed buy_sell_stock_2 question

---
+++ best_time_buy_sell_stock_2/solution.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        int maxProfit(vector<int>& prices) {
+            int maxP = 0;
+            for(int i = 1; i < prices.size(); i++){
+                if(prices[i-1] < prices[i]){
+                    maxP += prices[i] - prices[i-1];
+                }
+            }
+            return maxP;
+        }
+};
