3db3d0964d1043a31e7a23f01103b7669156cf55
TravHaran <ratnahat@mcmaster.ca>
2023-12-19 14:23:22 -0500

    updated minimum_size_subarray_sum question

--- sliding_window/minimum_size_subarray_sum/solution.cpp
+++ sliding_window/minimum_size_subarray_sum/solution.cpp
@@ -5,21 +5,18 @@ using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int min_len = INT32_MAX;
         int l = 0;
+        int minLen = INT32_MAX;
         int sum = 0;
         for(int r = 0; r < nums.size(); r++){
             sum += nums[r];
             while(sum >= target){
-                min_len = min(min_len, (r - l)+1);
+                minLen = min(minLen, (r - l) + 1);
                 sum -= nums[l];
                 l++;
             }
         }
-        if (min_len==INT32_MAX)
-            return 0;
-        return min_len==INT32_MAX ? 0: min_len;
-        
+        return minLen==INT32_MAX ? 0 : minLen;
     }
 };
 
