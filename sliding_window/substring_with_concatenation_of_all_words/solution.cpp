0a964ade66e012ed7f40a7f36dfb9b44fbcfa550
TravHaran <ratnahat@mcmaster.ca>
2023-12-20 11:03:41 -0500

    completed substring_with_concatenation_of_all_words

---
+++ sliding_window/substring_with_concatenation_of_all_words/solution.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> result;
+        unordered_map<string, int> word_map;
+        string concat;
+        //create frequency map of words
+        for(const string word : words){
+            concat += word;
+            word_map.count(word)==0 ? word_map[word] = 1 : word_map[word]++;
+        }
+        if(s==concat)
+            return {0};
+        int num_words = words.size();
+        int word_len = words[0].size();
+        int r;
+        int start;
+        string cur_word;
+        unordered_map<string, int> temp_word_map;
+        unordered_set<string> prev_seen; // cache to store previously seen unique concatenations
+        
+        for(int l = 0; l < s.size(); l++){
+            if(prev_seen.count(s.substr(l, word_len*num_words))==1){
+                result.push_back(l);
+                continue;
+            }
+            cur_word = s.substr(l, word_len);
+            if(word_map.count(cur_word)==1){
+                concat = "";
+                start = l;
+                temp_word_map = word_map;
+                while(l <= (s.size() - word_len) && temp_word_map.count(cur_word)==1){
+                    concat += cur_word;
+                    temp_word_map[cur_word] == 1 ? temp_word_map.erase(cur_word) : temp_word_map[cur_word]--;
+                    l += word_len;
+                    cur_word = s.substr(l, word_len);
+                }
+                if(temp_word_map.empty()){
+                    result.push_back(start);
+                    prev_seen.insert(concat);
+                } 
+                l = start;
+            }
+        }
+        return result;
+    }
+};
+
+int main(){
+    Solution solution;
+    string s = "wordgoodgoodgoodbestword";
+    vector<string> words = {"word","good","best","word"};
+    vector<int> result = solution.findSubstring(s, words);
+    for(const auto el : result){
+        cout<<el<<endl;
+    }
+}
