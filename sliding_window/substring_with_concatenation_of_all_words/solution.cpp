74ef5c3819d8e8dc145ff8d8f395772b86b5db77
TravHaran <ratnahat@mcmaster.ca>
2023-12-20 13:17:09 -0500

    updated answer for substring_with_concatenation_of_all_words question

--- sliding_window/substring_with_concatenation_of_all_words/solution.cpp
+++ sliding_window/substring_with_concatenation_of_all_words/solution.cpp
@@ -9,25 +9,27 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> result;
+        int num_words = words.size();
+        int word_len = words[0].size();
+        if(s.size() < word_len*num_words)
+            return result;
         unordered_map<string, int> word_map;
         string concat;
         //create frequency map of words
         for(const string word : words){
             concat += word;
-            word_map.count(word)==0 ? word_map[word] = 1 : word_map[word]++;
+            word_map[word]++;
         }
-        if(s==concat)
+        if(s==concat) // handle edge case in which large word list is equal to string
             return {0};
-        int num_words = words.size();
-        int word_len = words[0].size();
-        int r;
+        
         int start;
         string cur_word;
         unordered_map<string, int> temp_word_map;
         unordered_set<string> prev_seen; // cache to store previously seen unique concatenations
         
         for(int l = 0; l < s.size(); l++){
-            if(prev_seen.count(s.substr(l, word_len*num_words))==1){
+            if(prev_seen.count(s.substr(l, word_len*num_words))==1){ 
                 result.push_back(l);
                 continue;
             }
@@ -38,7 +40,8 @@ public:
                 temp_word_map = word_map;
                 while(l <= (s.size() - word_len) && temp_word_map.count(cur_word)==1){
                     concat += cur_word;
-                    temp_word_map[cur_word] == 1 ? temp_word_map.erase(cur_word) : temp_word_map[cur_word]--;
+                    temp_word_map[cur_word]--;
+                    if(temp_word_map[cur_word] == 0) temp_word_map.erase(cur_word);
                     l += word_len;
                     cur_word = s.substr(l, word_len);
                 }
@@ -46,7 +49,7 @@ public:
                     result.push_back(start);
                     prev_seen.insert(concat);
                 } 
-                l = start;
+                l = start; // reset l to start so that we can increment to next element of string
             }
         }
         return result;
