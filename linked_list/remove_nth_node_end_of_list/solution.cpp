06eb9897a4c5f254ae2866ba0404629ee9961bd5
TravHaran <ratnahat@mcmaster.ca>
2023-12-30 10:10:51 -0500

    completed remove_nth_node_end_of_list question

---
+++ linked_list/remove_nth_node_end_of_list/solution.cpp
@@ -0,0 +1,34 @@
+using namespace std;
+
+class Solution {
+private:
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode() : val(0), next(nullptr) {}
+        ListNode(int x) : val(x), next(nullptr) {}
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    }; 
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* cur = head;
+        int length = 1;
+        while(cur->next){
+            cur = cur->next;
+            length++;
+        }
+        int target = length - n;
+        cur = head;
+        target--;
+        while(target>0){
+            cur = cur->next;
+            target--;
+        }
+        if(cur==head && target<0) // we need to remove the head
+            head = head->next;
+        else { //we dont need to remove the head
+            cur->next = cur->next->next;
+        }
+        return head; 
+    }
+};
