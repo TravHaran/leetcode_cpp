66bcef2e69c166b43b0de13bec375f65984d89df
TravHaran <ratnahat@mcmaster.ca>
2024-01-16 00:47:27 -0500

    completed search_insert_position question

---
+++ binary_search/search_insert_position/solution.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+    // Time: O(nlogn)
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int mid;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid] < target){
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+};
