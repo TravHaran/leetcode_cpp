ea2b8ec8f68340816e5e562e05e264f98a7436bb
TravHaran <ratnahat@mcmaster.ca>
2024-01-16 14:21:35 -0500

    completed find_min_rotated_sorted_array question

---
+++ binary_search/find_min_rotated_sorted_array/solution.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size()-1;
+        int mid;
+        while(l <= r){
+            mid = (l+r)/2;
+            if(nums[mid] < nums[r]){
+                r = mid;
+            } else{
+                l = mid+1; 
+            }
+        }
+        return nums[mid];
+    }
+};
